20baf5f50d668896ff9bf27999efe8bf688dd275
Ina-dang <ina9377@gmail.com>
2022-09-07 12:03:20 +0900

    내장객체공부하기

---
+++ programmers/a220907/poketmon.js
@@ -0,0 +1,14 @@
+function solution(nums) {
+    var answer = 0;
+    console.log(nums)
+    
+    var poke = [...new Set(nums)];
+    console.log(poke.length)
+    if (nums.length >= 1 && nums.length <= 10000 && nums.length % 2 == 0){
+
+    }
+    return answer;
+}
+
+solution([3,4,1,6,6,6]);
+
