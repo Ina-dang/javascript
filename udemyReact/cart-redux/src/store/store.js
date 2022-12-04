82668a01e39dd9136d7477f3c5b7454705f615de
Ina-dang <ina9377@gmail.com>
2022-12-04 19:04:39 +0900

    카트 수량, 가격조절

--- udemyReact/cart-redux/src/store/store.js
+++ udemyReact/cart-redux/src/store/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { uiSlice } from "./index";
+import { uiSlice, cartSlice } from "./index";
 
 const store = configureStore({
-  reducer: { ui: uiSlice.reducer },
+  reducer: { ui: uiSlice.reducer, cart: cartSlice.reducer },
 });
 
 export default store;
