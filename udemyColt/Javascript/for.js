3ba887538599914e0525af98ab00ea6a8a7d4bb4
Ina-dang <ina9377@gmail.com>
2023-01-11 17:07:24 +0900

    udemy function

---
+++ udemyColt/Javascript/for.js
@@ -0,0 +1,12 @@
+const square = {
+  area(x) {
+    return x * x;
+  },
+
+  perimeter(x) {
+    return x * 4;
+  },
+};
+
+console.log(square.area(10));
+console.log(square.perimeter(10));
---
+++ udemyColt/Javascript/higherOrder.js
@@ -0,0 +1,6 @@
+function rollDice() {
+  const roll = Math.floor(Math.random() * 6 + 1);
+  console.log(roll);
+}
+
+rollDice();
