425f824feeaafa8be14fdd4599ef2d2a1db30869
Ina-dang <ina9377@gmail.com>
2023-03-06 21:28:54 +0900

    요청에 대한 응답방식  app.use

--- udemyColt/Javascript/20230306/FirstApp/index.js
+++ udemyColt/Javascript/20230306/FirstApp/index.js
@@ -2,8 +2,11 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.use(() => {
+app.use((req, res) => {
   console.log('새로운 요청 획득');
+  // res.send('요청을 받았습니다. 응답을 보냅니다.');
+  // res.send({ color: 'red' });
+  res.send('<h1>여기는 나의 웹페이지</h1>');
 });
 
 app.listen(port, () => {
