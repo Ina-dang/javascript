127feacddd130faacce93a7fa0b091e14d8c9295
Ina-dang <ina9377@gmail.com>
2023-03-06 21:49:45 +0900

    경로 매개변수. 라우터 뒤에 req.params 사용

--- udemyColt/Javascript/20230306/FirstApp/index.js
+++ udemyColt/Javascript/20230306/FirstApp/index.js
@@ -9,6 +9,22 @@ const port = 3000;
 //   res.send('<h1>여기는 나의 웹페이지</h1>');
 // });
 
+app.get('/', (req, res) => {
+  console.log('메인 페이지!!');
+  res.send('메인');
+});
+
+app.get('/r/:subreddit', (req, res) => {
+  console.log(`서브 URL! /r/${req.params.subreddit}`);
+  res.send(`${req.params.subreddit} 맛집`);
+});
+
+app.get('/r/:subreddit/:postId', (req, res) => {
+  const { subreddit, postId } = req.params;
+  console.log(`서브 URL! ${subreddit}/${postId}`);
+  res.send(`${req.params.subreddit} 맛집 추천 :: ${postId}`);
+});
+
 app.get('/cats', (req, res) => {
   console.log('고양이 페이지!!');
   res.send('야옹');
@@ -22,11 +38,6 @@ app.post('/cats', (req, res) => {
 app.get('/dogs', (req, res) => {
   console.log('강아지 페이지!!');
   res.send('월월');
-});
-
-app.get('/', (req, res) => {
-  console.log('메인 페이지!!');
-  res.send('메인');
 });
 
 app.get('*', (req, res) => {
