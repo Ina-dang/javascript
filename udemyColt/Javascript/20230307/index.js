5ebf6de68946b1cdf4c14357e34b4977c4e9e622
Ina-dang <ina9377@gmail.com>
2023-03-07 21:36:41 +0900

    보간문자 사용해서 ejs파일로 변수 값 보내기

--- udemyColt/Javascript/20230307/index.js
+++ udemyColt/Javascript/20230307/index.js
@@ -1,11 +1,18 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 
 //EJS지정 (외에도 여러가지 가능)
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (req, res) => {
   res.render('home.ejs');
+});
+
+app.get('/random', (req, res) => {
+  const num = Math.floor(Math.random() * 10 + 1);
+  res.render('random', { random: num });
 });
 
 app.listen(3000, () => {
