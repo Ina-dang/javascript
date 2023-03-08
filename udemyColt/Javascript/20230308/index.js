ba11a40d6844bd69a636daa24bd7957bec1042c1
Ina-dang <ina9377@gmail.com>
2023-03-08 21:34:20 +0900

    RESTful index

--- udemyColt/Javascript/20230308/index.js
+++ udemyColt/Javascript/20230308/index.js
@@ -1,5 +1,38 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+const comments = [
+  {
+    username: 'Todd',
+    comment: 'lol that is so funny!',
+  },
+  {
+    username: 'Skyler',
+    comment: 'I love it!',
+  },
+  {
+    username: 'Todd',
+    comment: 'lol that is so funny!',
+  },
+  {
+    username: 'Skyler',
+    comment: 'I love it!',
+  },
+  {
+    username: 'Todd',
+    comment: 'lol that is so funny!',
+  },
+];
+
+app.get('/comments', (req, res) => {
+  res.render('comments/index', { comments });
+});
 
 app.get('/tacos', (req, res) => {
   res.send('GET /tacos 응답중..');
