11a5605281e6879a5d093455efb9e9d5603b0f32
Ina-dang <ina9377@gmail.com>
2023-03-13 21:36:36 +0900

    camp Model 생성

--- udemyColt/FindCampingPlace/app.js
+++ udemyColt/FindCampingPlace/app.js
@@ -1,14 +1,33 @@
 const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Database Connected'));
+
 const app = express();
 const port = 3000;
-const path = require('path');
+
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.listen(port, () => {
-  console.log('3000 Connected...');
-});
-
 app.get('/', (req, res) => {
   res.render('home');
+});
+
+app.get('/makecampground', async (req, res) => {
+  const camp = new Campground({
+    title: 'My Backyard',
+  });
+  await camp.save();
+  res.send(camp);
+  console.log(camp);
+});
+
+app.listen(port, () => {
+  console.log('3000 Connected...');
 });
