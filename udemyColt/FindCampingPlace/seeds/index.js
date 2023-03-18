7bd1f29ec3083f9b25a4b3829f3b571ca9f09c7a
Ina-dang <ina9377@gmail.com>
2023-03-18 22:04:22 +0900

    unsplash로 임시 이미지, 간세네로 임시상세설명 추가

--- udemyColt/FindCampingPlace/seeds/index.js
+++ udemyColt/FindCampingPlace/seeds/index.js
@@ -14,9 +14,14 @@ const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${makingName(descriptors)} ${makingName(places)}`,
+      image: 'https://source.unsplash.com/collection/483251',
+      description:
+        '군인은 현역을 면한 후가 아니면 국무총리로 임명될 수 없다. 농업생산성의 제고와 농지의 합리적인 이용을 위하거나 불가피한 사정으로 발생하는 농지의 임대차와 위탁경영은 법률이 정하는 바에 의하여 인정된다.',
+      price: price,
     });
     await camp.save();
     console.log(camp);
